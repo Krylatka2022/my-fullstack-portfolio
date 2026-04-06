876ad8c5ee2023655930629342e7e90b614b3e64
Daria Krushelnitskaya <Dasha_fr_Russia@mail.ru>
2026-04-06 20:14:57 +0300

    OOP-2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const getBaseUrl = () => {
   return window.location.hostname === "localhost"
     ? "http://localhost:8000/api/projects"
-    : "https://krylatka-dev.ru"; // Указываем путь к статичному JSON на Beget
+    : // : "https://krylatka-dev.ru"; // Указываем путь к статичному JSON на Beget
+      "https://krylatka-dev.ru/backend/api/projects";
 };
 
 export const fetchProjects = async () => {
